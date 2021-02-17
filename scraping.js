ee7c221720b4ec3c806d18e91b067e0f962f5cd1
Diego <ramirezvargasdiegoalejandro2@gmail.com>
2021-02-16 20:38:57 -0500

    Web scraping dialogflow NoMatch

--- scraping.js
+++ scraping.js
@@ -1,14 +1,10 @@
 require('dotenv').config();
-
-const cheerio = require('cheerio');
 //Email y contraseña de gmail para entrar a Dialogflow
 input={
     username: process.env.EMAIL,
     password: process.env.PASSWORD
 };
-
-
-const navigateUpToAnalytics= async (page,browser)=>{
+const navigateUpToHistory= async (page,browser)=>{
     await page.goto('https://accounts.google.com/o/oauth2/auth/identifier?redirect_uri=storagerelay%3A%2F%2Fhttps%2Fdialogflow.cloud.google.com%3Fid%3Dauth978297&response_type=permission%20id_token&scope=email%20profile%20openid%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fcloud-platform%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Factions.builder%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fassistant%20https%3A%2F%2Fwww.googleapis.com%2Fauth%2Flogging.write&openid.realm&client_id=1016740739912-2dfjqgvhb5aqn920vvarkfniio1cahlh.apps.googleusercontent.com&ss_domain=https%3A%2F%2Fdialogflow.cloud.google.com&fetch_basic_profile=true&gsiwebsdk=2&flowName=GeneralOAuthFlow'); 
     await page.type('#identifierId', input.username);
     await page.click('button.VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ.VfPpkd-LgbsSe-OWXEXe-dgl2Hf.nCP5yc.AjY5Oe.DuMIQc.qIypjc.TrZEUc');
@@ -20,115 +16,124 @@ const navigateUpToAnalytics= async (page,browser)=>{
     await page.close();
     let page2 = await browser.newPage();
     await page2.setCookie(...cookies);
-    // Open the page as a logged-in user
+    
     await page2.goto('https://dialogflow.cloud.google.com/#/login',{ waitUntil: 'networkidle0'});
     await page2.click('button.md-raised.md-primary.md-btn-login.md-btn-google.md-button.md-ink-ripple');
     await page2.waitForTimeout(5000);
     await page2.click('button.btn-icon.toggle-left-panel');
-    await page2.waitForTimeout(1000);
-    await page2.click('#link-analytics');
-    await page2.waitForTimeout(3000);
-    await page2.click('md-tab-item[tabindex="-1"]');
+    await page2.waitForTimeout(2000);
+    const buttonPanel=await page2.evaluate(async()=>{
+      let resp;
+      function timeout(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
+      const nameBotScraping="BotEmpaquesDesarrollo";//NOMBRE DEL BOT AL CUAL SE LE VA A REALIZAR EL WEB SACRAPING
+      const nameBot=document.querySelector("a.dropdown-toggle.md-ink-ripple").children[0];
+      if(nameBot.textContent.trim()!==nameBotScraping){
+        nameBot.click();
+        await timeout(1000);
+        const nameBots=document.querySelectorAll("ul.dropdown-menu>li.ng-scope>a")
+        nameBots.forEach(bot=>{
+          if(bot.children[0].textContent===nameBotScraping){
+            bot.click();
+          }
+        })
+        resp=true;
+      }else{
+        resp=false;
+      }
+      return resp
+    })
+    if(buttonPanel===true){
+      await page2.waitForTimeout(6000);
+      await page2.click('button.btn-icon.toggle-left-panel');
+    }
+    await page2.waitForTimeout(2000);
+    await page2.click('#link-history');
     return page2
 }
-const puppeteer = require('puppeteer-extra')
-// Enable stealth plugin with all evasions
-puppeteer.use(require('puppeteer-extra-plugin-stealth')())
-;(async () => {
-  // Launch the browser in headless mode and set up a page.
-  const browser = await puppeteer.launch({
-    args: ['--no-sandbox'],
-    headless: true
+
+const adaptPageForScraping= async (page2)=>{
+  await page2.waitForTimeout(5000);
+  await page2.evaluate(()=>{
+    const dateStart='Jan 9, 2020';//FECHA DE INICIO PARA REVISAR NO MATCH
+    const dateEnd='Feb 16, 2021';//FECHA DE FIN PARA REVISAR NO MATCH
+    const dateInput=document.querySelectorAll("input.md-datepicker-input");
+    dateInput.forEach((element)=>{
+      element.value = "";
+    })
+    dateInput[0].value=dateStart;
+    dateInput[1].value=dateEnd;
+    console.log(dateInput[0])
+  });
+  await page2.type('input.md-datepicker-input', " ");
+  // await page.type('#identifierId', input.username);
+  await page2.waitForTimeout(2000);
+  await page2.evaluate(()=>{
+    document.querySelector('md-icon.refresh.material-icons.flex-50').click();
   })
-    const page = await browser.newPage()    
-    let page2=await navigateUpToAnalytics(page,browser);
-    await page2.waitForTimeout(10000);
-    
-    let data =await page2.evaluate(() => {
-        let sessions = document.querySelector('div[class="charts ng-scope"]').children[0].innerHTML;
-        let queriesPerSession=document.querySelector('div[class="charts ng-scope"]').children[1].innerHTML;
-        
-
-        return {
-            sessions:sessions,
-            queriesPerSession:queriesPerSession
-        };
+  await page2.waitForTimeout(4000);
+  await page2.click('div.filter-panel.layout-row>div.layout-row.flex-40>md-select.md-no-underline.sessions.ng-pristine.ng-valid.ng-empty');
+    await page2.evaluate(()=>{
+      let noMatch=document.querySelectorAll('md-content._md>md-option.md-ink-ripple>div.md-text');
+      noMatch.forEach(element => {
+        if (element.textContent==='No match conversations'){
+          element.click();
+          element.click();
+        }
       });
-    //console.log(data);
-    let $ = cheerio.load(data.sessions);
-    console.log('Sesiones en los ultimos 7 dias: '+$('number-display > span').text());
-    $ = cheerio.load(data.queriesPerSession);
-    console.log('Queries por sesión en los ultimos 7 dias: '+$('number-display > span').text());
+    });
 
+}
 
-    await page2.waitForTimeout(90000);
+const computeNumberOfNoMatch= async (page2)=>{
+  await page2.waitForTimeout(5000);
+  let Btndata=await page2.evaluate(async () => {
+    function timeout(ms) {
+      return new Promise(resolve => setTimeout(resolve, ms));
+    }
+    let contNoMatch=0;
+    let info=true
+    while(info===true){
+      const buttonNext=document.querySelector('div.page-nav-buttons.unselectable').children[1];
+      let classBtn=buttonNext.getAttribute('class');
+      if(classBtn==='next material-icons'){
+        buttonNext.click()
+        await timeout(2000)
+        console.log('click!!!!')
+        contNoMatch+=25
+        console.log("Available", contNoMatch)
+      }else{
+        console.log("NOT Available", contNoMatch)
+        info=false;
+      }
+    }
+    return contNoMatch
+  });
+  await page2.waitForTimeout(2000);
+  const BtndataFinal= await page2.evaluate(() => {
+    let contNoMatch=0;
+    contNoMatch+=document.querySelector('div.conversations-component').children.length;
+    return contNoMatch    
+  })
+  Btndata+=BtndataFinal;
+  console.log('La cantidad de no match es: '+Btndata);
+}
 
-    
-    await browser.close()
-})()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-input={
-    username: "ramirezvargasdiegoalejandro2@gmail.com",
-    password: "queleimporta1"
-};
 const puppeteer = require('puppeteer-extra')
-// Enable stealth plugin with all evasions
 puppeteer.use(require('puppeteer-extra-plugin-stealth')())
 ;(async () => {
-  // Launch the browser in headless mode and set up a page.
+  console.log('EJECUTANDO EL WEB SCRAPING, ESTO PUEDE TARDAR UN MOMENTO...');
   const browser = await puppeteer.launch({
     args: ['--no-sandbox'],
     headless: false
   })
-    const page = await browser.newPage()
-    await page.goto('https://dialogflow.cloud.google.com/#/login');
-    await page.waitForTimeout(2000);
-    await page.click('button.md-raised.md-primary.md-btn-login.md-btn-google.md-button.md-ink-ripple');
-    await page.waitForTimeout(90000);
-    //const pages = await browser.pages(); 
-    //const popup = pages[pages.length - 1];
-
-
-    await popup.type('#identifierId', input.username);
-    await popup.click('button.VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ.VfPpkd-LgbsSe-OWXEXe-dgl2Hf.nCP5yc.AjY5Oe.DuMIQc.qIypjc.TrZEUc');
-    await popup.waitForTimeout(2000);
-    await popup.type('input.whsOnd.zHQkBf', input.password);
-    await popup.click('button.VfPpkd-LgbsSe.VfPpkd-LgbsSe-OWXEXe-k8QpJ.VfPpkd-LgbsSe-OWXEXe-dgl2Hf.nCP5yc.AjY5Oe.DuMIQc.qIypjc.TrZEUc');
-
-    await page.waitForTimeout(50000);
+    const page = await browser.newPage()    
+    let page2=await navigateUpToHistory(page,browser);
+    await adaptPageForScraping(page2);
+    await computeNumberOfNoMatch(page2);
+   // await page2.waitForTimeout(300000);
     await browser.close()
-})()*/
+})()
+
 
